1731d8eb4d879aef9e82637abdacab510fdd82cc
Eric Dalessio <edalessio@gmail.com>
2024-01-24 17:12:14 -0500

    Added beautifulSoup

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ import datetime
 
 loosers_url = 'https://puppeteer-jibberish-deploy-cb9882febdc1.herokuapp.com'
 loosers_response = requests.get(loosers_url)
-result_json_content = loosers_response.json
+loosers_page = BeautifulSoup(loosers_response.text, 'html.parser')
+
+
+result_json_content = loosers_page.contents
 
 print("got this response from puppeteer: ")
 print(result_json_content)
@@ -16,4 +19,4 @@ if os.path.exists(loosers_json_filename):
   os.remove(loosers_json_filename)
 
 with open(loosers_json_filename, 'a') as loosers_json_file:
-  loosers_json_file.write(result_json_content.text)
+  loosers_json_file.write(result_json_content)
